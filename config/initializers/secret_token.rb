277fda81e6038b45d5ddd3b2459a63f81597bec0
Viko Nava <viko.nava@gmail.com>
2012-10-02 08:58:09 -0500

    Initial configuration with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusMTY::Application.config.secret_token = 'f477ac2c93031b61a8638aabb4a7f1d6fad57a075708f5518c74db77c5a839a9023152dcb5a67b238f0d64a71847552971881d442655eab08150d4073fdbf419'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CampusMTY::Application.config.session_store :cookie_store, key: '_CampusMTY_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CampusMTY::Application.config.session_store :active_record_store
